0b84329d984bdfafc1ce33e91f20475928c909a3
Alton Wimer <98047098+wimeraj30@users.noreply.github.com>
2022-01-19 18:27:45 +0000

    iteration 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-cost = 0
-sandwhich = input("Would you like a chicken, beef, or tofu sandwhich?:")
+cost = 0.00
+sandwhich = input("Would you like a chicken, beef, or tofu sandwhich?: ")
 
 if sandwhich == "chicken":
   print("You picked chicken")
@@ -10,3 +10,18 @@ elif sandwhich == "beef":
 else:
   print("you picked tofu")
   cost = 5.75
+beverage = input("Would you like a beverage?: ")
+if beverage == "yes":
+  size = input("Small, Medium, or Large?: ")
+  if size == "small":
+    print("You picked small")
+    cost = cost + 1.00
+  elif size == "medium":
+    print("You picked medium")
+    cost = cost + 1.75
+  else:
+    print("You picked large")
+    cost = cost + 2.25
+else:
+  print("You picked no beverage")
+print("Your total cost is $"+str(cost))
