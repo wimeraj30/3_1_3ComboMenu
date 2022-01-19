893bd556afa0bc6236d97bb10b4e16784afe328c
Alton Wimer <98047098+wimeraj30@users.noreply.github.com>
2022-01-19 18:13:12 +0000

    Iteration 1 tasks added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-chicken = 5.25
-beef = 6.25
-tofu = 5.75
+cost = 0
+sandwhich = input("Would you like a chicken, beef, or tofu sandwhich?:")
+
+if sandwhich == "chicken":
+  print("You picked chicken")
+  cost = 5.25
+elif sandwhich == "beef":
+  print("You picked beef")
+  cost = 6.25
+else:
+  print("you picked tofu")
+  cost = 5.75
